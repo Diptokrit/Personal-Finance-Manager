ee6da9c85f8afa402a306adecf9f6d01b760a9bc
Diptokrit Chowdhury <diptokritc@gmail.com>
2025-12-23 13:53:04 +0530

    Built Expense class for managing data from csv file
    Updated the Expense class to improve string representation and added methods for CSV conversion.

--- src/expense.py
+++ src/expense.py
@@ -1,10 +1,36 @@
 class Expense:
-    def __init__(self, amount, category, date, description):
+    #__init__ is the constructor which automatically excecutes whenever we create an expense object
+    #For example: exp = Expense(200, "Food", "2024-01-01", "Snacks")
+
+    def __init__(self,amount , category , date , description):
+        
         self.amount = float(amount)
         self.category = category
         self.date = date
         self.description = description
 
+    #dunder method or the magic method
     def __str__(self):
-        return f"{self.date} | {self.category}: ₹{self.amount:.2f} - {self.description}"
+       return f" Date:{self.date} | Category: {self.category}  Rs: {self.amount:.2f} - {self.description}"
+
+# Convert the expense object into a list for saving into csv
+    def to_row(self):  
+        return [self.date , self.category , self.amount , self.description]
+
+# Covert rows into an Expense object 
+#  row = [date, category, amount, description] 
+
+#for making objects using classes from csv files
+    @classmethod 
+    def from_row(cls , row):
+#multi packing stores 4 parameters seperately from the row
+        date , category , amount , description = row
+        
+        #creates objects 
+        return cls(amount , category , date , description)
+#exp1 = Expense(200 , "Food" , "2024-01-01","Snacks")
+#exp2 = Expense(500 , "Gaming" , "2025-12-22" , "Assasins Creed")
+    
+#print(exp1)
+#print(exp2)
 
