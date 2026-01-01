b48163dfd69c069be4aff0038d7d93c94f3c4648
Diptokrit Chowdhury <diptokritc@gmail.com>
2026-01-01 20:26:35 +0530

    Refactored menu.py for consistency and clarity

--- src/menu.py
+++ src/menu.py
@@ -1,71 +1,57 @@
 from expense import Expense
-from file_manager import load_expenses ,  save_expenses
+from file_manager import load_expenses, save_expenses
+
 
 def show_menu():
-    print("========= Personal Finance Manager =========")
+    print("\n========= Personal Finance Manager =========")
     print("1. Add Expense")
     print("2. View Expenses")
     print("3. Exit")
 
-def add_expenses():
+
+def add_expense():
     print("\n--- Add New Expense ---")
 
-#Taking input from the user
     amount = input("Enter amount: ")
-    category = input(" Enter category: ")
+    category = input("Enter category: ")
     date = input("Enter date (YYYY-MM-DD): ")
     description = input("Enter description: ")
-#now to create object out of these data:
 
-    expense = Expense(amount , category , date , description)
+    expense = Expense(amount, category, date, description)
 
-    #Load existing expenses
     expenses = load_expenses()
-
-    #Add new expense
     expenses.append(expense)
-
-    #Save back to csv
     save_expenses(expenses)
 
-    print("Expense succesfull !")
+    print("✅ Expense added successfully!")
+
 
 def view_expenses():
-    #reloading all previous data (csv - obj - list)
     expenses = load_expenses()
 
-    #case : no expense found
     if not expenses:
-        print("No expenses found")
-    return
+        print("No expenses found.")
+        return
 
-    #print every expense
     for exp in expenses:
         print(exp)
 
 
 def menu_loop():
-    #Infinite loop
-    while true:
+    while True:
         show_menu()
-        choice = input("Enter your choice")
+        choice = input("Enter your choice (1-3): ")
 
         if choice == "1":
-            add_expenses()
+            add_expense()
         elif choice == "2":
             view_expenses()
         elif choice == "3":
-            print("Exciting.. Goodbye!")
+            print("Exiting... Goodbye!")
             break
         else:
-            print(" Invalid choise .Please try again")    
-    
+            print("❌ Invalid choice. Please try again.")
 
 
-
-
-
-
-
-
-
+# TEMPORARY (for testing only)
+#menu_loop()
