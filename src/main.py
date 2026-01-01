5748409fb154c2b08f209f74de55d2f95566e484
Diptokrit Chowdhury <diptokritc@gmail.com>
2026-01-01 23:13:30 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,49 @@
-from menu import menu_loop
+from file_manager import load_expenses, add_expense
+from reports import total_expense, category_summary
+from utils import format_currency
+from expense import Expense
 
-menu_loop()
+
+
+def show_menu():
+    print("\nPersonal Finance Manager")
+    print("1. View Expense Report")
+    print("2. Add Expense")
+    print("3. Exit")
+
+
+def view_report():
+    expenses = load_expenses()
+    total = total_expense(expenses)
+    summary = category_summary(expenses)
+
+    print("\nExpense Report")
+    print("----------------")
+    print(f"Total Expense: {format_currency(total)}\n")
+
+    print("Category-wise Breakdown:")
+    for category, amount in summary.items():
+        print(f"- {category}: {format_currency(amount)}")
+
+
+def main():
+    while True:
+        show_menu()
+        choice = input("Enter your choice (1/2/3): ")
+
+        if choice == "1":
+            view_report()
+
+        elif choice == "2":
+            add_expense()
+
+        elif choice == "3":
+            print("Exiting... Goodbye!")
+            break
+
+        else:
+            print("Invalid choice. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
